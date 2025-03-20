fe27fb2f2af09399a83b1bd87aa4fcf0069a5048
Max Dobler <dobler@synyx.de>
2025-03-20 08:05:47 +0100

    Example eventcatalog
    * AsyncApi generated by springwolf
    * OpenApi generated by springdoc

--- eventcatalog.config.js
+++ eventcatalog.config.js
@@ -1,52 +1,59 @@
-import path from 'path';
-import url from 'url';
+import path from "path";
+import url from "url";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 /** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
 export default {
-  title: 'EventCatalog',
-  tagline: 'This internal platform provides a comprehensive view of our event-driven architecture across all systems. Use this portal to discover existing domains, explore services and their dependencies, and understand the message contracts that connect our infrastructure',
-  organizationName: 'synyx GmbH & Co. KG',
-  homepageLink: 'https://eventcatalog.dev/',
-  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  title: "EventCatalog",
+  tagline:
+    "This internal platform provides a comprehensive view of our event-driven architecture across all systems. Use this portal to discover existing domains, explore services and their dependencies, and understand the message contracts that connect our infrastructure",
+  organizationName: "synyx GmbH & Co. KG",
+  homepageLink: "https://eventcatalog.dev/",
   // By default set to false, add true to get urls ending in /
   trailingSlash: false,
   // Change to make the base url of the site different, by default https://{website}.com/docs,
   // changing to /company would be https://{website}.com/company/docs,
-  base: '/',
+  base: "/",
   logo: {
-    alt: 'EventCatalog Logo',
-    src: '/logo.png',
-    text: 'EventCatalog',
+    alt: "EventCatalog Logo",
+    src: "/logo.png",
+    text: "EventCatalog",
   },
   generators: [
     [
-      '@eventcatalog/generator-asyncapi',
+      "@eventcatalog/generator-asyncapi",
       {
         services: [
-          { path: path.join(__dirname, 'asyncapi-files', 'orders-service.yml'), owners: ['order-management'], id: 'Orders Service' },
+          {
+            path: path.join(
+              __dirname,
+              "asyncapi-files",
+              "springwolf-example.json"
+            ),
+            // path: "http://localhost:8080/springwolf/docs",
+            id: "springwolf-example",
+          },
         ],
-        domain: { id: 'orders', name: 'Orders', version: '0.0.1' },
+        domain: { id: "springwolf", name: "Springwolf", version: "0.0.1" },
       },
     ],
     [
-      '@eventcatalog/generator-asyncapi',
+      "@eventcatalog/generator-openapi",
       {
         services: [
-          { path: path.join(__dirname, 'asyncapi-files', 'payment-service.yml'), owners: ['order-management'], id: 'Payment Service' },
+          {
+            path: path.join(
+              __dirname,
+              "openapi-files",
+              "springwolf-example.json"
+            ),
+            // path: "http://localhost:8080/v3/api-docs",
+            id: "springwolf-example",
+          },
         ],
-        domain: { id: 'payment', name: 'Payment', version: '0.0.1' },
-      },
-    ],
-    [
-      '@eventcatalog/generator-asyncapi',
-      {
-        services: [
-          { path: path.join(__dirname, 'asyncapi-files', 'user-service.yml'), owners: ['user-management'], id: 'User Service' },
-        ],
-        domain: { id: 'user-domain', name: 'User Domain', version: '0.0.1' },
-        debug: true
+        domain: { id: "springwolf", name: "Springwolf", version: "0.0.1" },
+        debug: true,
       },
     ],
   ],
@@ -54,15 +61,15 @@ export default {
     sidebar: {
       // TREE_VIEW will render the DOCS as a tree view and map your file system folder structure
       // LIST_VIEW will render the DOCS that look familiar to API documentation websites
-      type: 'LIST_VIEW'
+      type: "LIST_VIEW",
     },
   },
   // Enable RSS feed for your eventcatalog
   rss: {
     enabled: true,
     // number of items to include in the feed per resource (event, service, etc)
-    limit: 20
+    limit: 20,
   },
   // required random generated id used by eventcatalog
-  cId: 'c0d1c81d-df9c-4339-8c2d-b3fdc36fe440'
+  cId: "c0d1c81d-df9c-4339-8c2d-b3fdc36fe440",
 };
